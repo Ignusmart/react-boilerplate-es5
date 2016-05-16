0347131f233e3ba09e53046dffeeb2db3a71ba54
ignusmart <ignusmart@gmail.com>
2016-05-16 09:51:07 -0500

    initial config

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var TwnelApp = require('./components/MyApp.react');
+var React = require('react');
+var ReactDom = require('react-dom');
+
+ReactDom.render(
+  <MyApp />,
+  document.getElementById('react')
+);
